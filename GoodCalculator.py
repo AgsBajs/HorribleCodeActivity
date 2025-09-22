062ebb7dfbacf8802eb1c3e746d5b6d48936d762
Agampreet <bajajagampreet@gmail.com>
2025-09-22 14:52:00 -0400

    Finished Good Calculator and functioning input loop

--- GoodCalculator.py
+++ GoodCalculator.py
@@ -8,4 +8,39 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError
     return a / b
+
+print("Select Operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+
+while True:
+    choice = input()
+    if choice in ('1','2','3','4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "yes":
+            print("Select Operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+        elif next_calculation == "no":
+            print("Thank you. Goodbye.")
+            break
+
+    else:
+        print("Invalid Input")
