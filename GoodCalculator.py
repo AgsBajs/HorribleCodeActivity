643a7f835bcc65f2fb36eb2f229caab6b24655e7
Agampreet <bajajagampreet@gmail.com>
2025-09-22 15:03:21 -0400

    Added new function for clean code and non repetitive code

--- GoodCalculator.py
+++ GoodCalculator.py
@@ -23,35 +23,42 @@ def divide(a, b):
         raise ZeroDivisionError
     return a / b
 
-print("Select Operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
+def show_menu():
+    print("\nSelect Operation:")
+    print("1. Add\n2. Subtract\n3. Multiply\n4. Divide\nq. Quit")
 
-while True:
-    choice = input()
-    if choice in ('1','2','3','4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+def main():
+    show_menu()
+    while True:
+        choice = input()
+        if choice in ('1','2','3','4'):
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+                continue
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            elif choice == '4':
+                print(num1, "/", num2, "=", divide(num1, num2))
 
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "yes":
-            print("Select Operation.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide")
-        elif next_calculation == "no":
-            print("Thank you. Goodbye.")
-            break
+            next_calculation = input("Let's do next calculation? (yes/no): ")
+            if next_calculation == "yes":
+                show_menu()
+            elif next_calculation == "no":
+                print("Thank you. Goodbye.")
+                break
 
-    else:
-        print("Invalid Input")
+        else:
+            print("Invalid Input")
+
+if __name__ == '__main__':
+    main()
